8f8986b14b164bad4c4154aedee750bfd1ba96e6
Russell Osborne <russell@burningpony.com>
2015-10-20 09:34:22 -0400

    Updating to App Model 2.0 Preview

--- lib/omniauth/strategies/office365.rb
+++ lib/omniauth/strategies/office365.rb
@@ -3,25 +3,38 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class Office365 < OmniAuth::Strategies::OAuth2
+      BASE_SCOPE_URL = 'https://graph.windows.net/'
+      BASE_SCOPES = %w(openid offline_access)
+      DEFAULT_SCOPE = 'openid offline_access'
       option :name, :office365
 
-      option :client_options, {
-        site:          'https://outlook.office365.com/',
-        token_url:     'https://login.windows.net/common/oauth2/token',
-        authorize_url: 'https://login.windows.net/common/oauth2/authorize'
-      }
+      option :client_options, site:          'https://login.microsoftonline.com',
+                              token_url:     '/common/oauth2/v2.0/token',
+                              authorize_url: '/common/oauth2/v2.0/authorize'
 
-      option :authorize_params, {
-        resource: 'https://graph.windows.net/'
-      }
+      def authorize_params
+        super.tap do |params|
+          raw_scope = params[:scope] || DEFAULT_SCOPE
+          scope_list = raw_scope.split(' ').map { |item| item.split(',') }.flatten
+          scope_list.map! { |s| s =~ /^https?:\/\// || BASE_SCOPES.include?(s) ? s : "#{BASE_SCOPE_URL}#{s}" }
+          params[:scope] = scope_list.join(' ')
 
-      uid { raw_info["objectId"] }
+          params[:response_type] = 'code' if params[:response_type].nil?
+          params[:response_mode] = 'query' if params[:response_mode].nil?
+
+          session['omniauth.state'] = params[:state] if params['state']
+        end
+      end
+
+      uid do
+        raw_info['sub']
+      end
 
       info do
         {
-          'email' => raw_info["userPrincipalName"],
-          'name' => [raw_info["givenName"], raw_info["surname"]].join(' '),
-          'nickname' => raw_info["displayName"]
+          name: raw_info['name'],
+          email: raw_info['preferred_username'],
+          oid: raw_info['oid']
         }
       end
 
@@ -32,7 +45,8 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get(authorize_params.resource + 'Me?api-version=1.5').parsed
+        # it's all here in JWT http://msdn.microsoft.com/en-us/library/azure/dn195587.aspx
+        @raw_info ||= ::JWT.decode(access_token['id_token'], nil, false).first
       end
     end
   end
